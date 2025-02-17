8bd9924cd20329dd0f96b2b7b44a616cd6cf49cd
Farzin Vatani <farzinvatani@outlook.com>
2025-02-18 00:17:17 +0330

    Implement resizing

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,26 @@
 const PRIME_1: u128 = 5003;
 const PRIME_2: u128 = 5281;
+const INITIAL_BASE_SIZE: usize = 53;
 
-#[derive(Clone, PartialEq, Eq)]
+struct Prime(usize);
+
+#[derive(Clone, PartialEq, Eq, Debug)]
 struct HashTableItem {
     key: String,
     value: String,
 }
 
-#[derive(Clone, PartialEq, Eq)]
+#[derive(Clone, PartialEq, Eq, Debug)]
 enum HashItemType {
     Removed,
     Item(HashTableItem),
 }
 
+#[derive(Debug)]
 struct HashTable {
     size: usize,
     count: usize,
+    base_size: usize,
     items: Vec<Option<HashItemType>>,
 }
 
@@ -30,10 +35,15 @@ impl HashTableItem {
 
 impl HashTable {
     fn new() -> Self {
+        HashTable::new_sized(INITIAL_BASE_SIZE)
+    }
+
+    fn new_sized(base_size: usize) -> Self {
         Self {
-            size: 53,
+            size: base_size,
             count: 0,
-            items: vec![None; 53],
+            base_size: Prime(base_size).next_prime(),
+            items: vec![None; base_size],
         }
     }
 
@@ -56,7 +66,41 @@ impl HashTable {
         (hash_a + (attempt * (hash_b + 1))) % num_buckets
     }
 
+    fn resize(&mut self, base_size: usize) {
+        if base_size < INITIAL_BASE_SIZE {
+            return;
+        }
+
+        let mut new_hash_table = HashTable::new_sized(base_size);
+
+        for i in 0..self.size {
+            let item = &self.items[i];
+            if let Some(HashItemType::Item(val)) = item {
+                new_hash_table.insert(&val.key, &val.value);
+            }
+        }
+
+        self.base_size = new_hash_table.base_size;
+        self.count = new_hash_table.count;
+        self.size = new_hash_table.size;
+    }
+
+    #[inline]
+    fn resize_up(&mut self) {
+        self.resize(self.base_size * 2);
+    }
+
+    #[inline]
+    fn resize_down(&mut self) {
+        self.resize(self.base_size / 2);
+    }
+
     pub fn insert(&mut self, key: &str, value: &str) {
+        let load = self.count * 100 / self.size;
+        if load > 70 {
+            self.resize_up();
+        }
+
         let item = HashTableItem::new(key, value);
         let mut index = HashTable::get_hash(&item.key, self.size, 0);
         let mut current_item = &self.items[index];
@@ -102,6 +146,11 @@ impl HashTable {
     }
 
     pub fn delete(&mut self, key: &str) {
+        let load = self.count * 100 / self.size;
+        if load < 10 {
+            self.resize_down();
+        }
+
         let mut index = HashTable::get_hash(key, self.size, 0);
         let mut item = &self.items[index];
 
@@ -119,6 +168,38 @@ impl HashTable {
         }
 
         self.count -= 1;
+    }
+}
+
+impl Prime {
+    fn is_prime(&self) -> Option<bool> {
+        if self.0 < 2 {
+            return None;
+        }
+
+        if self.0 < 4 {
+            return Some(true);
+        }
+
+        if self.0 % 2 == 0 {
+            return Some(false);
+        }
+
+        for i in (3..=self.0.isqrt()).step_by(2) {
+            if self.0 % i == 0 {
+                return Some(false);
+            }
+        }
+
+        return Some(true);
+    }
+
+    fn next_prime(&mut self) -> usize {
+        while self.is_prime() != Some(true) {
+            self.0 += 1;
+        }
+
+        self.0
     }
 }
 
